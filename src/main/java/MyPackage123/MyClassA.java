10a8b9c7cacc6476263c31bfe95d78446bdf39d9
DesanDinsanda <desandinsanda@gmail.com>
2025-01-01 15:00:41 +0530

    test

---
+++ src/main/java/MyPackage123/MyClassA.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyPackage123;
+
+/**
+ *
+ * @author DELL
+ */
+public class MyClassA {
+  
+    
+}
---
+++ src/main/java/MyPackage456/MyClassB.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyPackage456;
+import MyPackage123.*;
+
+/**
+ *
+ * @author DELL
+ */
+public class MyClassB {
+    MyClassA a = new  MyClassA();
+}
